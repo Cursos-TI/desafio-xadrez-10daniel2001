f5159cde2cb3f0aae9b6d691ee8c5911cea1c9cd
10daniel2001 <cd38668cd@gmail.com>
2025-07-24 23:50:22 +0000

    Nivel novato

--- xadrez.c
+++ xadrez.c
@@ -3,9 +3,9 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-#include <stdio.h> //carlos daniel 
+#include <stdio.h> //carlos daniel 10/07/2025
 
-int main(){
+int main(){ 
     //variaveis
     int torre,bispo=1;
     int rainha=1;
